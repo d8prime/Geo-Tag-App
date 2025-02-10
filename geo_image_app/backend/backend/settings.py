182650279ca7ef098c5d1793b312e0893471e6dc
Dinesh Gurram <129779133+d8prime@users.noreply.github.com>
2025-02-11 00:30:21 +0530

    Updated settings.py

--- geo_image_app/backend/backend/settings.py
+++ geo_image_app/backend/backend/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lk-_k+l8(_3&*93+tb9&2znx%q@w_dg9@q*j)o02i35mv*khe^'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
